db92ce24cbbade827122361666b6d33208cdc759
toliyapayal6-bit <toliyapayal6@gmail.com>
2026-06-29 10:35:43 +0530

    Create if-else.py

---
+++ if-else.py
@@ -0,0 +1,243 @@
+# # name = input("Enter tne name:")
+# Age = int(input("Enter the age :"))
+
+# if(Age >= 18 ):
+#     print("show movies")
+# else:
+#     print("Not show movies")
+    
+    
+
+# name = input("Enter the Name :")
+# age = int(input("Enter the Age: "))
+# heigt = float(input("Enter the Heighn:"))
+# student = bool(input("Enter the Is_student: "))
+
+# if(name == "Abult" or name == "minor"):
+#     print("The Person is", name)
+# else:
+#     print("Not This Person:")
+
+
+# num = input("enter the num :")
+# if (num %2 == 0):
+# # if (num % 2 != 0):
+#     print("even num")
+# else:
+#     print("odd num")
+
+tuple = (20,40,34,67,22)
+average = sum(tuple) / len(tuple)
+if (average >= 50):
+    print("PASS")
+else:
+    print("Fail")
+    
+# list = [10,22,4,5,10,33,7,9,4,10]
+# set = set(list)
+# print(set)
+
+
+# list = [1.5, 7.9, 8.5, 4.3, 10.3]
+# # print(max(list))
+# highest = list[0]
+# for x in list:
+#     if x > highest:
+#         highest = x
+#     else:
+#         pass
+# print("max num in list :", highest)
+
+
+# tuple = ( True,False,True)
+# a = tuple.count(True)
+# print(a)
+
+
+
+
+# list = [10,22,3,4,10,2,4,3,22]
+# a1 = set(list)
+# print(a1)
+
+
+
+# num_tuple = (10,40,20,50,60,34,56,26)
+# for x in num_tuple:
+#     if (x > 30):
+#         print(x)
+
+
+ 
+# dict = {
+#     "username": "ravi" ,
+#     "password":123
+# }
+# name = input("Enter the name :")
+# password = input("Enter the Password: ")
+# if(name == dict["username"] and password == dict["password"]):
+#     print("Login in valind")
+# else:
+#     print("Login in invalid")
+    
+    
+
+# dic = {"A'bad": 44.5, "Mumbai":36.4,"pune":40, "Delhi": 47.4}
+# temp = dic.values()
+# print(temp)
+# if ( dic["A'bad"] > 40):
+#     print(dic["A'bad"])
+# if(dic["Mumbai"]>40):
+#     print(dic["Mumbai"])
+# if(dic["pune"]>40):
+#     print(dic["pune"])
+# if(dic["Delhi"]> 40):
+#     print(dic["Delhi"])
+
+
+
+
+# list = [2,-8,6,10,-4,0,-1,3,-4,0]
+# positive = 0
+# negative = 0
+# zero = 0
+# if(list[0]> 0):
+#     print("positive:",list[0])
+# elif(list[0]<0):
+#     print("Negative",list[0])
+# elif(list[0] == 0):
+#     print("zero",list[0]) 
+# if(list[1]>0):
+#     print("Positive",list[1])
+# elif(list[1]<0):
+#     print("negative",list[1])
+# elif(list[1] ==0):
+#     print("Zero",list[1])
+# if(list[2]>0):
+#     print("Positive",list[2])
+# elif(list[2]<0):
+#     print("negative",list[2])
+# elif(list[2] ==0):
+#     print("Zero",list[2])
+# if(list[3]>0):
+#     print("Positive",list[3])
+# elif(list[3]<0):
+#     print("negative",list[3])
+# elif(list[3] ==0):
+#     print("Zero",list[3])
+# if(list[4]>0):
+#     print("Positive",list[4])
+# elif(list[4]<0):
+#     print("negative",list[4])
+# elif(list[4] ==0):
+#     print("Zero",list[4])
+# if(list[5]>0):
+#     print("Positive",list[5])
+# elif(list[5]<0):
+#     print("negative",list[5])
+# elif(list[5] ==0):
+#     print("Zero",list[5])
+# if(list[6]>0):
+#     print("Positive",list[6])
+# elif(list[6]<0):
+#     print("negative",list[6])
+# elif(list[6] ==0):
+#     print("Zero",list[6])
+# if(list[7]>0):
+#     print("Positive",list[7])
+# elif(list[7]<0):
+#     print("negative",list[7])
+# elif(list[7] ==0):
+#     print("Zero",list[7])
+# if(list[8]>0):
+#     print("Positive",list[8])
+# elif(list[8]<0):
+#     print("negative",list[8])
+# elif(list[8] ==0):
+#     print("Zero",list[8])
+# if(list[9]>0):
+#     print("Positive",list[9])
+# elif(list[9]<0):
+#     print("negative",list[9])
+# elif(list[9] ==0):
+#     print("Zero",list[9])
+
+
+
+# list = [ "a","g","l","o","t"]
+# vowels = ["a","e","i","o","u"]
+# if (list[0] == vowels):
+#     print("vowels:",list[0])
+# else:
+#     print("consonats:", list[0])
+
+
+
+name = 'ravi','meet'
+list  = [50,35,85,77,60]
+total = sum(list)
+ave = sum(list)|len(list)
+print(total)
+print(average)
+
+if(list[0] >= 90):
+    print("A")
+elif( list[0] >= 75):
+    print("B")
+elif(list[0] >= 50):
+    print("C")
+else:
+    print("F")
+if(list[1] >= 90):
+    print("A")
+elif( list[1] >= 75):
+    print("B")
+elif(list[1] >= 50):
+    print("C")
+else:
+    print("F")
+if(list[2] >= 90):
+    print("A")
+elif( list[2] >= 75):
+    print("B")
+elif(list[2] >= 50):
+    print("C")
+else:
+    print("F")
+if(list[3] >= 90):
+    print("A")
+elif( list[3] >= 75):
+    print("B")
+elif(list[3] >= 50):
+    print("C")
+else:
+    print("F")
+if(list[4] >= 90):
+    print("A")
+elif( list[4] >= 75):
+    print("B")
+elif(list[4] >= 50):
+    print("C")
+else:
+    print("F")
+
+
+
+
+# dic = {
+#     'ravi':68,
+#     'meet':45,
+#     'jeel':50
+# }
+# if (dic['ravi'] >= 50):
+#     print("Ravi:","pass")
+# else:
+#     print("Ravi:","Fail")
+# if (dic['meet'] >= 50):
+#     print("meet:","pass")
+# else:
+#     print("meet:","Fail")
+# if (dic['jeel'] >= 50):
+#     print("jeel:","pass")
+# else:
+#     print("jeel:","Fail")
