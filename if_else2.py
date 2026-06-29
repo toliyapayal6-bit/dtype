425113ca741855281daebd8853cee146dacf008b
toliyapayal6-bit <toliyapayal6@gmail.com>
2026-06-29 10:37:59 +0530

    Create if_else2.py

---
+++ if_else2.py
@@ -0,0 +1,39 @@
+list = [3,5,7,8,10,2]
+small = min(list)
+print("small num:",small)
+large = max(list)
+print("Large num :", large)
+a = sum(list)
+average = sum(list)|len(list)
+print("Avreg :", average)
+
+dict = {
+    "small":small,
+    "large":large,
+    "avrege":average
+}
+print(dict)
+
+
+# tuple = (7,10,3,4)
+# if (tuple[0] % 2 == 0):
+#     print(tuple[0],"even num", )
+# else:
+#     print(tuple[0],"odd num")
+# if( tuple[1]%2 == 0):
+#     print(tuple[1], "even num")
+# else:
+#     print(tuple[1],"odd num")
+# if(tuple[2] % 2 == 0):
+#     print(tuple[2],"even num")
+# else:
+#     print(tuple[2],"odd num")
+# if tuple[3] % 2 == 0:
+#     print(tuple[3],"even num")
+# else:
+#     print(tuple[3], "odd num")
+
+# dic = {
+#     "tuple": tuple
+# }
+# print(dic)
